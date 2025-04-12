93bd7815a5222ff8f1c2ef0ec17051a300022e47
brotherjoo <khj01173@naver.com>
2025-04-12 19:13:07 +0900

    database connect

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const path = require("path");
 const session = require("express-session");
+
 const { sequelize } = require("./models");
+
+const indexRouter = require("./src/main/router");
 
 require("dotenv").config();
 
 const app = express();
 app.set("port", process.env.PORT || 8080);
 
-// sequelize
-//     .sync({ force: true })
-//     .then(() => {
-//         console.log("데이터베이스 연결 성공");
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//     });
+sequelize
+    .sync({ force: false })
+    .then(() => {
+        console.log("데이터베이스 연결 성공");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -36,9 +39,7 @@ app.use(
     })
 );
 
-app.get("/", (req, res, next) => {
-    res.json({ d: "ok" });
-});
+app.use("/", indexRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
@@ -49,6 +50,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+
     res.status(err.status || 500);
     res.render("error");
 });
